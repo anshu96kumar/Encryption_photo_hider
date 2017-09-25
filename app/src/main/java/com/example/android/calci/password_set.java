4c3cefbf6ae09227812a9f79de4955733ee0e5e6
anshu96kumar <anshu.96.kumar@gmail.com>
2017-09-26 02:20:41 +0530

    pattern added

---
+++ app/src/main/java/com/example/android/calci/password_set.java
@@ -0,0 +1,19 @@
+package com.example.android.calci;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class password_set extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_set);
+    }
+
+
+}
--- app/src/main/java/com/example/android/calci/pattern.java
+++ app/src/main/java/com/example/android/calci/pattern.java
@@ -1,7 +1,11 @@
 package com.example.android.calci;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class pattern extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class pattern extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pattern);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.setting){
+            startActivity(new Intent(pattern.this,password_set.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
