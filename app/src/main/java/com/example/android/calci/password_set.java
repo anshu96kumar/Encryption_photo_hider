ffdcde0506e44daf3aa2ccfa0340715bc5d20212
anshu96kumar <anshu.96.kumar@gmail.com>
2017-11-06 00:15:05 +0530

    pattern added

--- app/src/main/java/com/example/android/calci/password_set.java
+++ app/src/main/java/com/example/android/calci/password_set.java
@@ -3,17 +3,28 @@ package com.example.android.calci;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class password_set extends AppCompatActivity {
+
+    TextView user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_set);
+
+        user = (TextView) findViewById(R.id.account_text);
+
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(password_set.this,User_info.class));
+            }
+        });
     }
+
 
 
 }
